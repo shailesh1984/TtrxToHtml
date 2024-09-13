cf66a71f96adde187b4473abf9136c8a7910f498
Shailesh M <shailesh.smile@gmail.com>
2024-09-13 23:09:39 +0530

    Initial Commit

---
+++ src/TtrxToHtml.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Linq;
+using TtrxToHtml.Console;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var sourceDirectory = @"C:\Personal\Work\TtrxToHtml\src\TtrxToHtml.Console";
+        string json = TrxHelper.CombineAllTrxFilesToOneTrx(sourceDirectory);
+    }
+}
+
+// To convert JSON text contained in string json into an XML node
+//XmlDocument doc = JsonConvert.DeserializeXmlNode(json);
+
+
---
+++ src/TtrxToHtml.Console/TrxHelper.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace TtrxToHtml.Console
+{
+    public static class TrxHelper
+    {
+        public static string CombineAllTrxFilesToOneTrx(string sourceDirectory)
+        {
+            var trxFiles = Directory.EnumerateFiles(sourceDirectory,
+                                            "*", SearchOption.AllDirectories)
+                   .Where(s => s.EndsWith(".trx") && s.Count(c => c == '.') == 2)
+                   .ToList();
+
+            XDocument doc = new();
+            XElement rootElement = new("TestResults");
+
+            foreach (var trxFile in trxFiles)
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.Load(trxFile);
+                var xElem = XElement.Parse(xmlDoc.LastChild.OuterXml);
+
+                rootElement.Add(new XElement(xElem));
+            }
+            doc.Add(rootElement);
+
+            //doc.Save(@"C:\Personal\Work\TtrxToHtml\src\TtrxToHtml.Console\CombineAllTrxFilesToOneTrx.trx");
+
+            XmlDocument singleTrxXdoc = new();
+            singleTrxXdoc.Load(doc.CreateReader());
+
+            foreach (XmlNode node in singleTrxXdoc)
+            {
+                if (node.NodeType == XmlNodeType.XmlDeclaration)
+                {
+                    singleTrxXdoc.RemoveChild(node);
+                }
+            }
+
+            string json = JsonConvert.SerializeXmlNode(singleTrxXdoc, Newtonsoft.Json.Formatting.Indented);
+
+            File.WriteAllText(@"C:\Personal\Work\TtrxToHtml\src\TtrxToHtml.Console\ConvertedTrxToJson.json", json);
+
+            return json;
+        }
+    }
+}

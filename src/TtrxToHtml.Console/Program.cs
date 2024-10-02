4ecbd679aa3816b5e003a427b5865219d1637da9
Shailesh M <shailesh.smile@gmail.com>
2024-10-02 23:21:06 +0530

    combined unittestresult and unittest objects

--- src/TtrxToHtml.Console/Program.cs
+++ src/TtrxToHtml.Console/Program.cs
@@ -7,14 +7,14 @@
         var sourceDirectory = @"C:\Personal\Work\TtrxToHtml\src\TtrxToHtml.Console"; // need to modify
         string json = TrxHelper.CombineAllTrxFilesToOneTrx(sourceDirectory);
 
-        var model = JsonConvert.DeserializeObject<JsonData>(json)!;
+        var testResult = JsonConvert.DeserializeObject<JsonData>(json)!;
 
         var engine = new RazorLightEngineBuilder()
             .UseEmbeddedResourcesProject(typeof(Program).Assembly, "TtrxToHtml.Console.Templates")
             .UseMemoryCachingProvider()
             .Build();
 
-        string html = await engine.CompileRenderAsync("JsonData", model);
+        string html = await engine.CompileRenderAsync("JsonData", testResult);
 
         var path = AppDomain.CurrentDomain.BaseDirectory;
         var dateTime = DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss");
@@ -26,42 +26,10 @@
         if (!exists)
             Directory.CreateDirectory(directoryPath);
 
-        var testReportFile = Path.Combine(directoryPath, @"TestReportFile_"+ dateTime + ".html");
+        var testReportFile = Path.Combine(directoryPath, @"TestReportFile_" + dateTime + ".html");
         await File.WriteAllTextAsync(testReportFile, html);
         Process.Start(@"cmd.exe ", $@"/c {testReportFile}");
     }
-
-
-    //public static string ReadEmbeddedResource(Assembly assembly, string resourceName)
-    //{
-    //    using (Stream resourceStream = assembly.GetManifestResourceStream(resourceName))
-    //    using (StreamReader reader = new StreamReader(resourceStream))
-    //    {
-    //        return reader.ReadToEnd();
-    //    }
-    //}
-
-    //internal static string GenerateHtml<T>(string resourceName, T model)
-    //{
-    //    var rootType = typeof(RootTypeForEmbeddedResources);
-    //    var rootTypeAssembly = rootType.Assembly;
-    //    var rootTypeNamespace = rootType.Namespace;
-
-    //    var layoutResource = ReadEmbeddedResource(rootTypeAssembly, rootTypeNamespace + "._Layout.cshtml");
-    //    var engine = new RazorLightEngineBuilder()
-    //        .UseEmbeddedResourcesProject(typeof(RootTypeForEmbeddedResources).Assembly, typeof(RootTypeForEmbeddedResources).Namespace)
-    //        .AddDynamicTemplates(new Dictionary<string, string>()
-    //            {
-    //                    {"_Layout.cshtml",layoutResource },
-    //            })
-    //        .SetOperatingAssembly(operatingAssembly)
-    //        .UseMemoryCachingProvider()
-    //        .EnableDebugMode()
-    //        .Build();
-
-    //    string html = engine.CompileRenderAsync<T>(resourceName, model).Result;
-    //    return html;
-    //}
 }
 
 // To convert JSON text contained in string json into an XML node

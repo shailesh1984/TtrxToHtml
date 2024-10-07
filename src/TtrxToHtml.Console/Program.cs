e144600cadae1296edafeedf1b934393bada70c3
Shailesh M <shailesh.smile@gmail.com>
2024-10-07 20:41:31 +0530

    Modified index cshtml file

--- src/TtrxToHtml.Console/Program.cs
+++ src/TtrxToHtml.Console/Program.cs
@@ -1,7 +1,8 @@
 ﻿public class Program
 {
-    private const string TemplatesFolder = "Templates";
+    private const string TEMPLATE_FOLDER = "Templates";
 
+    private const string OUTPUT_FILE_EXT = ".html";
     private static async Task Main(string[] args)
     {
         var sourceDirectory = @"C:\Personal\Work\TtrxToHtml\src\TtrxToHtml.Console"; // need to modify
@@ -26,7 +27,7 @@
         if (!exists)
             Directory.CreateDirectory(directoryPath);
 
-        var testReportFile = Path.Combine(directoryPath, @"TestReportFile_" + dateTime + ".html");
+        var testReportFile = Path.Combine(directoryPath, @"TestReportFile_" + dateTime + OUTPUT_FILE_EXT);
         await File.WriteAllTextAsync(testReportFile, html);
         Process.Start(@"cmd.exe ", $@"/c {testReportFile}");
     }

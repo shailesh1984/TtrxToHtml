f7aa919f82814f0cca2c2863f39bbfbdf4720abd
Shailesh M <shailesh.smile@gmail.com>
2024-10-08 22:17:58 +0530

    organized code into new class methods

---
+++ src/TtrxToHtml.Console/Helpers/CommandLineInterfaceHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace TtrxToHtml.Console.Helpers;
+
+public static class CommandLineInterfaceHelper
+{
+    public static string ArgumentsHelper(string[] args)
+    {
+        var arguments = CommandLineArgumentsHelper.ParseArguments(args);
+
+        if (arguments.Count == 0 || arguments.ContainsKey("-h") || arguments.ContainsKey("--help"))
+        {
+            CommandLineArgumentsHelper.PrintHelp();
+            //return;
+        }
+
+        if (arguments.ContainsKey("--info"))
+        {
+            CommandLineArgumentsHelper.PrintInfo();
+        }
+
+        var trxDirPath = string.Empty;
+        if (arguments.ContainsKey("-tdp") || arguments.ContainsKey("--trx-dir-path"))
+        {
+            if (arguments.TryGetValue("-tdp", out string? tdpValue))
+            {
+                trxDirPath = !string.IsNullOrEmpty(tdpValue) ? tdpValue : string.Empty;
+            }
+
+            if (arguments.TryGetValue("--trx-dir-path", out string? trxDirPathValue))
+            {
+                trxDirPath = !string.IsNullOrEmpty(trxDirPathValue) ? trxDirPathValue : string.Empty;
+            }
+
+            System.Console.WriteLine($"Trx source directory path: {trxDirPath}");
+        }
+
+        //if (arguments.TryGetValue("--output", out string? outputValue))
+        //{
+        //    System.Console.WriteLine($"Output file: {outputValue}");
+        //}
+
+        return trxDirPath;
+    }
+}
--- src/TtrxToHtml.Console/Program.cs
+++ src/TtrxToHtml.Console/Program.cs
@@ -7,39 +7,7 @@ public class Program
 {
     private static async Task Main(string[] args)
     {
-        var arguments = CommandLineArgumentsHelper.ParseArguments(args);
-
-        if (arguments.Count == 0 || arguments.ContainsKey("-h") || arguments.ContainsKey("--help"))
-        {
-            CommandLineArgumentsHelper.PrintHelp();
-            return;
-        }
-
-        if (arguments.ContainsKey("--info"))
-        {
-            CommandLineArgumentsHelper.PrintInfo();
-        }
-
-        var trxDirPath = string.Empty;
-        if (arguments.ContainsKey("-tdp") || arguments.ContainsKey("--trx-dir-path"))
-        {
-            if (arguments.TryGetValue("-tdp", out string? tdpValue))
-            {
-                trxDirPath = !string.IsNullOrEmpty(tdpValue) ? tdpValue : string.Empty;
-            }
-
-            if (arguments.TryGetValue("--trx-dir-path", out string? trxDirPathValue))
-            {
-                trxDirPath = !string.IsNullOrEmpty(trxDirPathValue) ? trxDirPathValue : string.Empty;
-            }
-
-            Console.WriteLine($"Trx source directory path: {trxDirPath}");
-        }
-
-        //if (arguments.TryGetValue("--output", out string? outputValue))
-        //{
-        //    Console.WriteLine($"Output file: {outputValue}");
-        //}
+        var trxDirPath = CommandLineInterfaceHelper.ArgumentsHelper(args);
 
         await GenerateTrxReportService.GenerateTrxReport(trxDirPath);
     }

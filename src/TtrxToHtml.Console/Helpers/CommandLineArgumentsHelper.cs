5bc5f17d3a0b550700505ba54a0684f05bc4c9c5
Shailesh M <shailesh.smile@gmail.com>
2024-10-09 17:03:02 +0530

    fixed cannot convert null literal to non-nullable reference type

--- src/TtrxToHtml.Console/Helpers/CommandLineArgumentsHelper.cs
+++ src/TtrxToHtml.Console/Helpers/CommandLineArgumentsHelper.cs
@@ -21,7 +21,7 @@ public static class CommandLineArgumentsHelper
             }
             else
             {
-                arguments[arg] = null;
+                arguments[arg] = null!;
             }
         }
 

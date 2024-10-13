9980966a07e7c0c551df759a7e3caa8bb149d4af
Shailesh M <shailesh.smile@gmail.com>
2024-10-13 17:14:26 +0530

    added namespace in program class

--- src/TtrxToHtml.Console/Program.cs
+++ src/TtrxToHtml.Console/Program.cs
@@ -1,15 +1,16 @@
-﻿/// <summary>
-/// Converting trx file to html format
-/// </summary>
-
-//[assembly: AssemblyVersionAttribute("1.0.0")]
+﻿//[assembly: AssemblyVersionAttribute("1.0.0")]
 
 //[HasSubCommands(typeof(NestedCommands))]
-public class Program
+namespace TtrxToHtml.Console
 {
-    private static async Task Main(string[] args)
+    /// <summary>
+    /// Converting trx file to html format
+    /// </summary>
+    public class Program
     {
-        #region Cocona
+        private static async Task Main(string[] args)
+        {
+            #region Cocona
             //CoconaApp.Run<Program>(
             //    args,
             //    options =>
@@ -27,46 +28,46 @@ public class Program
             //});
 
             //app.Run();
-        #endregion
+            #endregion
 
-        var builder = new ConfigurationBuilder();
-        builder.SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        
-        IConfigurationRoot configuration = builder.Build();
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-        var appSettings = new AppSettings();
-        configuration.GetSection("AppSettings").Bind(appSettings);
+            IConfigurationRoot configuration = builder.Build();
 
-        var commandLineInterfaceValues = CommandLineInterfaceHelper.ArgumentsHelper(appSettings, args);
+            var appSettings = new AppSettings();
+            configuration.GetSection("AppSettings").Bind(appSettings);
 
-        if (string.IsNullOrEmpty(commandLineInterfaceValues.TrxPath))
-        {
-            return;
+            var commandLineInterfaceValues = CommandLineInterfaceHelper.ArgumentsHelper(appSettings, args);
+
+            if (string.IsNullOrEmpty(commandLineInterfaceValues.TrxPath))
+            {
+                return;
+            }
+
+            var serviceProvider = CreateServices();
+            var generateTrxReportService = serviceProvider.GetService<IGenerateTrxReportService>()!;
+            var trxReport = await generateTrxReportService.ConvertTrxToHtmlAsync(appSettings, commandLineInterfaceValues);
+            var testReportFile = await generateTrxReportService.CreateHtmlAsync(trxReport, appSettings, commandLineInterfaceValues);
+            Process.Start(@"cmd.exe ", $"/c \"{testReportFile}\"");
         }
 
-        var serviceProvider = CreateServices();
+        private static ServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddScoped<IGenerateTrxReportService, GenerateTrxReportService>()
+                .BuildServiceProvider();
+        }
 
-        var generateTrxReportService = serviceProvider.GetService<IGenerateTrxReportService>()!;
-        var trxReport = await generateTrxReportService.ConvertTrxToHtmlAsync(appSettings, commandLineInterfaceValues);
-        var testReportFile = await generateTrxReportService.CreateHtmlAsync(trxReport, appSettings, commandLineInterfaceValues);
-        Process.Start(@"cmd.exe ", $"/c \"{testReportFile}\"");
+        //[Command(Description = "A dotnet cli program")]
+        //public void Hello(
+        //    [Option('u', Description = "Description of the option")] bool useOption,
+        //    [Argument(Description = "Description of the argument")] string user
+        //)
+        //{
+        //    var output = useOption ? $"{user}" : user;
+        //    Console.WriteLine($"Hello {output}");
+        //}
     }
-
-    private static ServiceProvider CreateServices()
-    {
-        return new ServiceCollection()
-            .AddScoped<IGenerateTrxReportService, GenerateTrxReportService>()
-            .BuildServiceProvider();
-    }
-
-    //[Command(Description = "A dotnet cli program")]
-    //public void Hello(
-    //    [Option('u', Description = "Description of the option")] bool useOption,
-    //    [Argument(Description = "Description of the argument")] string user
-    //)
-    //{
-    //    var output = useOption ? $"{user}" : user;
-    //    Console.WriteLine($"Hello {output}");
-    //}
 }
